72d93f1c2e131f63c1a932f413fe3782f67a65a0
Nguyen <emailcuaban@gmail.com>
2026-04-19 23:22:53 +0700

    hellooool

--- src/main/java/vn/fpoly/testdem/methods/MyUtil.java
+++ src/main/java/vn/fpoly/testdem/methods/MyUtil.java
@@ -1,4 +1,16 @@
 package vn.fpoly.testdem.methods;
 
 public class MyUtil {
+    public int tinhtongtu1denn(int n){
+        if (n<1){
+            throw new IllegalArgumentException("n phai lon hon 1");
+        }
+        int tong= 0;
+        for (int i = 1; i<=n;i++){
+            tong += i;
+        }
+        return tong;
+    }
+
 }
+
